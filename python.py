65fccdda05306689fa6cff3ad419c79f7f491cc4
Lucas Inocêncio <wartazer100@gmail.com>
2024-08-27 21:41:47 -0300

    Arquivos iniciais
    Arquivos iniciais da APS

---
+++ python.py
@@ -0,0 +1,8 @@
+caminho_arquivo = r'c:/wamp64/www/APS_1s_v1.0.0/10000_numbers.txt'
+
+with open(caminho_arquivo, 'r', encoding='utf-8') as arquivo:
+    conteudo = arquivo.read()
+
+for(i )
+
+print(conteudo)
