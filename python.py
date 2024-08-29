f5ca19ea57d81eaed58ed3b828e0ea0f3b333b76
Lucas Inocêncio <wartazer100@gmail.com>
2024-08-28 21:18:34 -0300

    Commit para criação de repo

--- python.py
+++ python.py
@@ -1,8 +1,12 @@
-caminho_arquivo = r'c:/wamp64/www/APS_1s_v1.0.0/10000_numbers.txt'
+# Obtém o caminho do arquivo que será lido
+caminho_arquivo = r'c:/wamp64/www/APS_1s_v1.0.0/1000_numbers.txt'
 
+# Abre o arquivo selecionado e verifica  o conteúdo
 with open(caminho_arquivo, 'r', encoding='utf-8') as arquivo:
-    conteudo = arquivo.read()
+    for i in caminho_arquivo:
+        leitura_linha = arquivo.readline()
+        print("\n "+ leitura_linha)       
+    #conteudo = arquivo.read()
 
-for(i )
 
-print(conteudo)
+#print(conteudo)
